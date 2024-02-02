b34908421482c34d0904dda55d9bf4aaeed0cffa
Nemanja <nemanja.milutinovic@endava.com>
2024-02-02 10:19:21 +0100

    Optimized WebDriverUtils class; Updated maven versions of Selenium and TestNG

--- src/main/java/utils/WebDriverUtils.java
+++ src/main/java/utils/WebDriverUtils.java
@@ -8,7 +8,6 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.remote.LocalFileDetector;
 import org.openqa.selenium.remote.RemoteWebDriver;
-import org.openqa.selenium.remote.SessionId;
 import org.testng.Assert;
 
 import java.net.MalformedURLException;
@@ -20,71 +19,94 @@ public class WebDriverUtils extends LoggerUtils {
     public static WebDriver setupDriver() {
         WebDriver driver = null;
 
-        String sBrowser = PropertiesUtils.getBrowser();
-        boolean bRemote = PropertiesUtils.getRemote();
-        boolean bHeadless = PropertiesUtils.getHeadless();
-        String sHubUrl = PropertiesUtils.getHubUrl();
+        String browser = PropertiesUtils.getBrowser();
+        boolean isRemote = PropertiesUtils.getRemote();
+        boolean isHeadless = PropertiesUtils.getHeadless();
+        String hubUrl = PropertiesUtils.getHubUrl();
 
-        logger.info("setUpDriver(" + sBrowser + ", Is Headless: " + bHeadless + ")");
+        logger.info("setUpDriver(" + browser + ", Is Headless: " + isHeadless + ")");
 
         try {
-            switch (sBrowser) {
-                case "firefox": {
-                    FirefoxOptions options = new FirefoxOptions();
-                    options.setHeadless(bHeadless);
-                    if (bRemote) {
-                        RemoteWebDriver remoteWebDriver = new RemoteWebDriver(new URL(sHubUrl), options);
-                        remoteWebDriver.setFileDetector(new LocalFileDetector());
-                        driver = remoteWebDriver;
-                    } else {
-                        driver = new FirefoxDriver(options);
-                        driver.get(PropertiesUtils.getBaseUrl());
-                    }
-                    break;
-                }
-                case "chrome": {
-                    ChromeOptions options = new ChromeOptions();
-                    options.setHeadless(bHeadless);
-                    if (bRemote) {
-                        RemoteWebDriver remoteWebDriver = new RemoteWebDriver(new URL(sHubUrl), options);
-                        remoteWebDriver.setFileDetector(new LocalFileDetector());
-                        driver = remoteWebDriver;
-                    } else {
-                        driver = new ChromeDriver(options);
-                        driver.get(PropertiesUtils.getBaseUrl());
-                    }
-                    break;
-                }
-                default: {
-                    Assert.fail("Cannot create driver1 Browser '" + sBrowser + "' is not recognized!");
-                }
+            if (isRemote) {
+                driver = createRemoteDriver(browser, isHeadless, hubUrl);
+            } else {
+                driver = createLocalDriver(browser, isHeadless);
+                driver.get(PropertiesUtils.getBaseUrl());
             }
         } catch (MalformedURLException e) {
-            Assert.fail("Cannot create driver! Path to browser '" + sBrowser + "' driver is not correct!");
+            Assert.fail("Cannot create driver! Path to browser '" + browser + "' driver is not correct!");
         }
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(Time.IMPLICIT_TIMEOUT));
-        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(Time.PAGE_LOAD_TIMEOUT));
-        driver.manage().timeouts().scriptTimeout(Duration.ofSeconds(Time.ASYNC_SCRIPT_TIMEOUT));
 
-        driver.manage().window().maximize();
+        setDriverTimeouts(driver);
+        maximizeWindow(driver);
         return driver;
     }
 
-    public static SessionId getSessionID(WebDriver driver) {
-        return ((RemoteWebDriver) driver).getSessionId();
+    private static WebDriver createRemoteDriver(String browser, boolean isHeadless, String hubUrl) throws MalformedURLException {
+        RemoteWebDriver remoteWebDriver;
+
+        switch (browser) {
+            case "firefox":
+                FirefoxOptions firefoxOptions = new FirefoxOptions();
+                if (isHeadless) {
+                    firefoxOptions.addArguments("--headless");
+                }
+                remoteWebDriver = new RemoteWebDriver(new URL(hubUrl), firefoxOptions);
+                break;
+            case "chrome":
+                ChromeOptions chromeOptions = new ChromeOptions();
+                if (isHeadless) {
+                    chromeOptions.addArguments("--headless");
+                }
+                remoteWebDriver = new RemoteWebDriver(new URL(hubUrl), chromeOptions);
+                break;
+            default:
+                Assert.fail("Cannot create driver! Browser '" + browser + "' is not recognized!");
+                return null; // Unreachable, but necessary for compiler
+        }
+
+        remoteWebDriver.setFileDetector(new LocalFileDetector());
+        return remoteWebDriver;
     }
 
-    public static boolean hasDriverQuit(WebDriver driver) {
-        if (driver != null) {
-            return getSessionID(driver) == null;
-        } else {
-            return true;
+
+    private static WebDriver createLocalDriver(String browser, boolean isHeadless) {
+        switch (browser) {
+            case "firefox":
+                FirefoxOptions firefoxOptions = new FirefoxOptions();
+                if (isHeadless) {
+                    firefoxOptions.addArguments("--headless");
+                }
+                return new FirefoxDriver(firefoxOptions);
+            case "chrome":
+                ChromeOptions chromeOptions = new ChromeOptions();
+                if (isHeadless) {
+                    chromeOptions.addArguments("--headless");
+                }
+                return new ChromeDriver(chromeOptions);
+            default:
+                Assert.fail("Cannot create driver! Browser '" + browser + "' is not recognized!");
+                return null; // Unreachable, but necessary for compiler
         }
+    }
+
+    private static void setDriverTimeouts(WebDriver driver) {
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(Time.IMPLICIT_TIMEOUT));
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(Time.PAGE_LOAD_TIMEOUT));
+        driver.manage().timeouts().scriptTimeout(Duration.ofSeconds(Time.ASYNC_SCRIPT_TIMEOUT));
+    }
+
+    private static void maximizeWindow(WebDriver driver) {
+        driver.manage().window().maximize();
     }
 
     public static void quitDriver(WebDriver driver) {
-        if (!hasDriverQuit(driver)) {
+        if (driver != null && !hasDriverQuit(driver)) {
             driver.quit();
         }
+    }
+
+    public static boolean hasDriverQuit(WebDriver driver) {
+        return driver == null || ((RemoteWebDriver) driver).getSessionId() == null;
     }
 }
